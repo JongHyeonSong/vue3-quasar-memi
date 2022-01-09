f74028d5cd48313e19afaa167564fdf1d036310d
DESKTOP-G23R95G\musicen <mapelamll@naver.com>
2022-01-09 12:01:55 +0900

    vue cli init

---
+++ src/components/Test.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const num = ref<number>(1);
+</script>
+
+<template>
+  <q-btn @click="num += 1">
+    <q-icon name="font_download" class="text-primary" style="font-size: 32px" />
+    <span>{{ num }}</span>
+  </q-btn>
+</template>
